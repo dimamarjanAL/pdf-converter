b7fb895e7feaaa0808b3e7250e9741981f217ee2
dimamarjanAL <dima.marjan@aleannlab.com>
2024-02-29 18:20:33 +0200

    Update CORS configuration and add Referrer-Policy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const config = require('./config');
+const express = require("express");
+const cors = require("cors");
+const config = require("./config");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://app.docuseer.com", "http://localhost:3000"],
+    methods: ["GET", "POST"],
+  }),
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+  next();
+});
+
 app.use(express.json());
 
-const routes = require('./routes');
-app.use('/api/', routes);
+const routes = require("./routes");
+app.use("/api/", routes);
 
 app.listen(config.port, () => {
   console.log(`Server is running on port ${config.port}`);
