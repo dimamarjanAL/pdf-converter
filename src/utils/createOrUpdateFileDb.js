9dc08fa6eb134624aa622856fdab8fe31fdaa6a4
AndrewOskolok <oscolok7@gmail.com>
2024-04-08 16:18:01 +0300

    update back

--- src/utils/createOrUpdateFileDb.js
+++ src/utils/createOrUpdateFileDb.js
@@ -1,44 +1,74 @@
 const moment = require("moment");
 const { supabaseClient } = require("../utils/supabaseClient");
 
+const determineQueryType = async ({ name, company, fromGoogle, folderId }) => {
+  if (fromGoogle) {
+    const { data, error } = await supabaseClient
+      .from("files")
+      .select()
+      .eq("name", name)
+      .eq("company", company)
+      .is("fromGoogle", fromGoogle)
+      .eq("folderId", folderId);
+
+    if (error) {
+      console.log(
+        "FILE SEARCHING ERROR",
+        "|",
+        moment().format("HH:mm:ss"),
+        "|",
+        error
+      );
+    }
+
+    return data[0];
+  } else {
+    const { data, error } = await supabaseClient
+      .from("files")
+      .select()
+      .eq("name", name)
+      .eq("company", company)
+      .is("fromGoogle", fromGoogle);
+
+    if (error) {
+      console.log(
+        "FILE SEARCHING ERROR",
+        "|",
+        moment().format("HH:mm:ss"),
+        "|",
+        error
+      );
+    }
+
+    return data[0];
+  }
+};
+
 exports.createOrUpdateFileDb = async ({
   name,
   company,
   fromSiteData = false,
   fromGoogle = false,
+  folderId = null,
   ...params
 }) => {
-  const { data: file, error } = await supabaseClient
-    .from("files")
-    .select()
-    .eq("name", name)
-    .eq("company", company)
-    .eq("fromGoogle", fromGoogle)
-    .eq("folderId", params.folderId);
+  let file = await determineQueryType({ name, company, fromGoogle, folderId });
 
-  if (error) {
-    console.log(
-      "FILE SEARCHING ERROR",
-      "|",
-      moment().format("HH:mm:ss"),
-      "|",
-      error
-    );
-  }
-
-  if (file?.length) {
-    if (file[0].fromGoogle) {
-      if (file[0].inProcessing && file[0].warningMsg) {
+  if (file) {
+    if (file.fromGoogle) {
+      if (file.inProcessing && file.warningMsg) {
         await supabaseClient
           .from("files")
           .update({
             warningMsg: null,
           })
-          .eq("id", file[0].id);
+          .eq("id", file.id);
       }
 
       return { data: file, error };
     } else {
+      await supabaseClient.from("documents").delete().eq("file", file.id);
+
       const { data, error } = await supabaseClient
         .from("files")
         .update([
@@ -47,10 +77,11 @@ exports.createOrUpdateFileDb = async ({
             name,
             fromSiteData,
             fromGoogle,
+            folderId,
             company,
           },
         ])
-        .eq("id", file[0].id)
+        .eq("id", file.id)
         .select("*,admin(email,slackToken)");
 
       return { data, error };
@@ -64,6 +95,7 @@ exports.createOrUpdateFileDb = async ({
           name,
           fromSiteData,
           fromGoogle,
+          folderId,
           company,
         },
       ])
